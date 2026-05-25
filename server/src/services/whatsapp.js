b0a1d94ad2675ab3fd6558c19e8538a2c1a47226
luffyheng <luffyheng@gmail.com>
2026-05-25 14:53:54 +0800

    perf: add Chromium memory-saving flags for 512MB Render instances

--- server/src/services/whatsapp.js
+++ server/src/services/whatsapp.js
@@ -87,6 +87,10 @@ export async function initWhatsApp() {
       args: [
         '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage',
         '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--disable-gpu',
+        '--disable-extensions', '--disable-software-rasterizer',
+        '--disable-background-timer-throttling', '--disable-backgrounding-occluded-windows',
+        '--disable-renderer-backgrounding', '--disable-features=TranslateUI',
+        '--disable-ipc-flooding-protection', '--single-process',
       ],
     },
   });
